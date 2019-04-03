7322527f3d3a457ebe747948c57397e83650aa94
DominiqueL234 <brunoowona12@gmail.com>
2019-04-03 15:07:16 +0100

    Travaux d'ajout d'identifcation sans reservation en cour

---
+++ src/Migrations/Version20190403130928.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190403130928 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_42C849554CC8505A ON reservation');
+        $this->addSql('DROP INDEX UNIQ_42C8495519EB6921 ON reservation');
+        $this->addSql('ALTER TABLE reservation CHANGE valide valide VARCHAR(20) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_42C849554CC8505A ON reservation (offre_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_42C8495519EB6921 ON reservation (client_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_42C849554CC8505A ON reservation');
+        $this->addSql('DROP INDEX UNIQ_42C8495519EB6921 ON reservation');
+        $this->addSql('ALTER TABLE reservation CHANGE valide valide VARCHAR(20) DEFAULT \'non\' COLLATE utf8mb4_unicode_ci');
+        $this->addSql('CREATE INDEX UNIQ_42C849554CC8505A ON reservation (offre_id)');
+        $this->addSql('CREATE INDEX UNIQ_42C8495519EB6921 ON reservation (client_id)');
+    }
+}
